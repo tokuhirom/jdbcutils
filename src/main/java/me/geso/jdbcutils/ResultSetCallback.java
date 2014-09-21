3605130ea4af8c032b640a1ce7e93add30eb4d2f
Tokuhiro Matsuno <tokuhirom@gmail.com>
2014-09-22 06:23:39 +0900

    javadoc

--- src/main/java/me/geso/jdbcutils/ResultSetCallback.java
+++ src/main/java/me/geso/jdbcutils/ResultSetCallback.java
@@ -4,7 +4,10 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 /**
- * Callback function, that receives ResultSet.
+ * Callback function, that receives ResultSet.<br>
+ * I need to implement this interface because ResultSetCallback accepts
+ * SQLException.<br>
+ * It makes easier to use JDBCUtils.
  *
  * @param <T>
  */
