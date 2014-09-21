cec0c969ec4727653614a7168917cd5543d0ae29
Tokuhiro Matsuno <tokuhirom@gmail.com>
2014-09-22 07:41:48 +0900

    Added builder class for Query.

---
+++ src/main/java/me/geso/jdbcutils/QueryBuilder.java
@@ -0,0 +1,72 @@
+package me.geso.jdbcutils;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * Builder class for {@code Query} class.
+ *
+ */
+public class QueryBuilder {
+	private final List<Object> parameters;
+	private final StringBuilder query;
+	private final String identifierQuoteString;
+
+	public QueryBuilder(final String identifierQuoteString) {
+		this.parameters = new ArrayList<>();
+		this.query = new StringBuilder();
+		this.identifierQuoteString = identifierQuoteString;
+	}
+
+	public QueryBuilder(final Connection connection) throws SQLException {
+		this(connection.getMetaData().getIdentifierQuoteString());
+	}
+
+	public QueryBuilder appendQuery(final String s) {
+		this.query.append(s);
+		return this;
+	}
+
+	public QueryBuilder appendIdentifier(final String identifier) {
+		this.query.append(JDBCUtils.quoteIdentifier(identifier,
+				identifierQuoteString));
+		return this;
+	}
+
+	public QueryBuilder append(final Query q) {
+		this.query.append(q.getSQL());
+		this.parameters.addAll(q.getParameters());
+		return this;
+	}
+
+	public QueryBuilder addParameter(final Object o) {
+		this.parameters.add(o);
+		return this;
+	}
+
+	public <T> QueryBuilder addParameters(final Collection<T> o) {
+		this.parameters.addAll(o);
+		return this;
+	}
+
+	public Query build() {
+		return new Query(new String(this.query), this.parameters);
+	}
+
+	public List<Object> getParameters() {
+		return parameters;
+	}
+
+	public StringBuilder getQuery() {
+		return query;
+	}
+
+	public QueryBuilder appendQueryAndParam(String s, Object o) {
+		this.appendQuery(s);
+		this.addParameter(o);
+		return this;
+	}
+}
---
+++ src/test/java/me/geso/jdbcutils/QueryBuilderTest.java
@@ -0,0 +1,72 @@
+package me.geso.jdbcutils;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import org.junit.Test;
+
+public class QueryBuilderTest {
+
+	@Test
+	public void test() {
+		Query q = new QueryBuilder("`").appendQuery("SELECT * FROM member")
+				.build();
+		assertThat(q.getSQL(), is("SELECT * FROM member"));
+		assertThat(q.getParameters(), is(Collections.emptyList()));
+	}
+
+	@Test
+	public void testAppendQueryAndParam() {
+		Query q = new QueryBuilder("`")
+				.appendQuery("SELECT * FROM member WHERE ")
+				.appendQueryAndParam("id=?", 9)
+				.build();
+		assertThat(q.getSQL(), is("SELECT * FROM member WHERE id=?"));
+		assertThat(q.getParameters(), is(Arrays.asList(9)));
+	}
+
+	@Test
+	public void testAddParameter() {
+		Query q = new QueryBuilder("`")
+				.appendQuery("SELECT * FROM member WHERE id=?")
+				.addParameter(9)
+				.build();
+		assertThat(q.getSQL(), is("SELECT * FROM member WHERE id=?"));
+		assertThat(q.getParameters(), is(Arrays.asList(9)));
+	}
+
+	@Test
+	public void testAddParameters() {
+		Query q = new QueryBuilder("`")
+				.appendQuery("SELECT * FROM member WHERE id IN (?,?,?)")
+				.addParameters(Arrays.asList(1,2,3))
+				.build();
+		assertThat(q.getSQL(), is("SELECT * FROM member WHERE id IN (?,?,?)"));
+		assertThat(q.getParameters(), is(Arrays.asList(1,2,3)));
+	}
+
+	@Test
+	public void testAppend() {
+		Query q = new QueryBuilder("`")
+				.appendQuery("SELECT * FROM member WHERE ")
+				.append(new Query("id=?", Arrays.asList(9)))
+				.build();
+		assertThat(q.getSQL(), is("SELECT * FROM member WHERE id=?"));
+		assertThat(q.getParameters(), is(Arrays.asList(9)));
+	}
+
+	@Test
+	public void testAppendIdentifier() {
+		Query q = new QueryBuilder("`")
+				.appendQuery("SELECT * FROM ")
+				.appendIdentifier("order")
+				.build();
+		assertThat(q.getSQL(), is("SELECT * FROM `order`"));
+		assertThat(q.getParameters(), is(Arrays.asList()));
+	}
+
+
+}
